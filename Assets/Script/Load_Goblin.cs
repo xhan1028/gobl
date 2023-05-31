7284518c9061a4f23f524a8130b660f5ae099e42
xhan1028 <127803431+xhan1028@users.noreply.github.com>
2023-05-31 16:12:21 +0900

    gggg

---
+++ Assets/Script/Load_Goblin.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load_Goblin : MonoBehaviour
+{
+    public void Goblin_Load()
+    {
+        SceneManager.LoadScene("Goblin_One");
+    }
+}
